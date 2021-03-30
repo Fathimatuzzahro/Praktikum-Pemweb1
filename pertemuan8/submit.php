b2ece030dc45e505fa67339f0cd048cdcf13ba47
Fathimatuzzahro <70746424+Fathimatuzzahro@users.noreply.github.com>
2021-03-30 14:04:04 +0700

    belajar membuat form pendaftaran

---
+++ pertemuan8/submit.php
@@ -0,0 +1,24 @@
+<!-- POST -->
+<?php
+    $jumlah = $_POST['jumlah'];
+    $barangpesanan = $_POST['barangpesanan'];
+    echo "Anda memesan " . $jumlah . " " . $barangpesanan . ".<br>";
+    echo "Terima kasih atas kesediaan Anda memesan barang dari kami!";
+?>
+<!-- 
+    Variabel jumlah didapatkan dari halaman pemesanan yaitu text field jumlah,
+    sedangkan untuk variable barangpesanan, nilainya didapatkan dari <select> yang ada di halaman pemesanan. 
+    Kemudian nilai dari variable ini ditampilkan ke layar
+-->
+
+<!-- GET -->
+<?php
+    $jumlah = $_GET['jumlah'];
+    $barangpesanan = $_GET['barangpesanan'];
+    echo "Anda memesan " . $jumlah . " " . $barangpesanan . ".<br>";
+    echo "Terima kasih atas kesediaan Anda memesan barang dari kami!";
+?>
+<!-- 
+    Perbedaanya adalah pada URL, Nampak nilai yang ada di dalam vaariabel barang pesanan 
+    yang bernilai map kertas dengan jumlah bernilai X.
+-->
